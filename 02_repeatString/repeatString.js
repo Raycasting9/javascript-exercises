5ec7daf3d7c87d96d7f304ec1c7917de0e92c18a
Muaz <169424072+Raycasting9@users.noreply.github.com>
2025-02-25 18:32:14 +0800

    Solved questions 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word,times) {
+
+    if (times < 0){
+        return 'ERROR';
+    }
+
+    let fullword = "";
+    for(let i=0; i < times; i++){
+        fullword += word;
+    }
+
+    return fullword;
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
 
+    if(typeof first != "number" || 
+        typeof last != "number" ||
+        !Number.isInteger(first) ||
+        !Number.isInteger(last) ||
+        first < 0 ||
+        last < 0
+    ){
+        return 'ERROR';
+    }
+
+    if(first>last){
+        [first, last] = [last, first];
+    }
+
+    let sum = 0;
+    for(let i = first;i <= last;i++){
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
